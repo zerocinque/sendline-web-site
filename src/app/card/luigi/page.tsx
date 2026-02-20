c2b4e74060ca30da574ff555d48caf02945919d5
miten <gigi.bottasso@gmail.com>
2026-02-20 12:37:24 +0100

    fix redirect

--- src/app/card/luigi/page.tsx
+++ src/app/card/luigi/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function LuigiCardPage() {
-  redirect("https://coversmart.it/public/profile.php?slug=luigi-bottasso");
+  redirect("https://coversmart.it/public/profile.php?slug=luigi-bottasso-1");
 }
